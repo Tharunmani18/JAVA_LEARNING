d95291455066d9dda094eb46c9c975e024bf9ff9
tharunMani <tharunmani1822@gmail.com>
2025-12-09 12:59:13 +0530

    removed unwanteds

--- features/Lambda.java
+++ features/Lambda.java
@@ -8,7 +8,7 @@ public class Lambda {
 
     public static void main(String[] args) {
         Finterface f = () ->"This is a lambda expression with functional interface";
-        System.out.println(f.sound());
+        System.out.println(f.sound()); 
     }
     
 }
